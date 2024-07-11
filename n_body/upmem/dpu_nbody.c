3baf5c5f18cd7ddead68a5239002fb5a98d8d6a1
Johannes Albrecht <johannes-a@t-online.de>
2024-07-11 15:46:35 +0000

    local_bodies as local varibale which is stored for all tasklets in the WRAM. only tasklet 0 handle the body object transfer between MRAM and WARM

--- n_body/upmem/dpu_nbody.c
+++ n_body/upmem/dpu_nbody.c
@@ -19,27 +19,20 @@ typedef struct {
     float padding;  // Auffüllbyte für 8-Byte-Ausrichtung
 } Body_f;
 
+#define nof_bodies 3
 
 __mram_noinit Body_f mram_bodies[3];
-__host float fx[3], fy[3], fz[3];
+float fx[3], fy[3], fz[3];
+
+Body_f local_bodies[3];
 
 BARRIER_INIT(my_barrier, 3);
 
-float fabsf(float x) {
-    return x < 0 ? -x : x;
-}
+// math methodes
+float fabsf(float x);
+float sqrtf(float x);
 
-float sqrtf(float x) {
-    float guess = x / 2.0f;
-    float epsilon = 0.00001f;
-
-    while (fabsf(guess * guess - x) > epsilon) {
-        guess = (guess + x / guess) / 2.0f;
-    }
-
-    return guess;
-}
-
+// Calculate the force between two bodies
 void computeForce_f(const Body_f *a, const Body_f *b, float *fx, float *fy, float *fz) {
     float dx = b->x - a->x;
     float dy = b->y - a->y;
@@ -59,40 +52,34 @@ int main() {
     // Initialisieren der perfcounter
     //perfcounter_config(COUNT_CYCLES, true);
     
-    Body_f local_bodies[3];
+    
 
     // Kopiere Daten von MRAM in WRAM
-    mram_read(mram_bodies, local_bodies, sizeof(local_bodies));
-    printf("Tasklet %d: Data copied from MRAM to WRAM\n", me());
+    if (me() == 0) {
+        mram_read(mram_bodies, local_bodies, sizeof(local_bodies));
+        printf("Tasklet %d: Data copied from MRAM to WRAM\n", me());
+    }
 
-    for (int step = 0; step < 10; ++step) {
+
+    // Berechnung der Kräfte und Aktualisierung der Positionen und Geschwindigkeiten
+    for (int step = 0; step < 2; ++step) {
         // Kräfte initialisieren
         if (me() == 0) {
             for (int i = 0; i < 3; ++i) {
                 fx[i] = fy[i] = fz[i] = 0.0f;
             }
-            //print tasklet id and force init done
-            printf("Tasklet %d: Force init done\n", me());
         }
-        
+
         barrier_wait(&my_barrier);
+
+
         
         // Kräfte berechnen
         for (int i = me(); i < 3; i += NR_TASKLETS) {
             for (int j = i + 1; j < 3; ++j) {
                 float fx_ij, fy_ij, fz_ij;
-                //computeForce_f(&bodies[i], &bodies[j], &fx_ij, &fy_ij, &fz_ij);
+                computeForce_f(&local_bodies[i], &local_bodies[j], &fx_ij, &fy_ij, &fz_ij);
                 
-                float dx = local_bodies[j].x - local_bodies[i].x;
-                float dy = local_bodies[j].y - local_bodies[i].y;
-                float dz = local_bodies[j].z - local_bodies[i].z;
-                float dist = sqrtf(dx * dx + dy * dy + dz * dz);
-                float force = (G * local_bodies[i].mass * local_bodies[j].mass) / (dist * dist * dist);
-
-                fx_ij = force * dx;
-                fy_ij = force * dy;
-                fz_ij = force * dz;
-
                 fx[i] += fx_ij;
                 fy[i] += fy_ij;
                 fz[i] += fz_ij;
@@ -100,6 +87,9 @@ int main() {
                 fy[j] -= fy_ij;
                 fz[j] -= fz_ij;
 
+                // print body index i and j and force calculation done
+                printf("Tasklet %d: Force calculation done for bodies %d <> %d\n", me(), i, j);
+                
             }
             
         }
@@ -131,12 +121,38 @@ int main() {
         printf("Tasklet %d: Position update done\n", me());
         barrier_wait(&my_barrier);
          
-        // Kopiere Daten von WRAM zurück in MRAM
-        if (me() == 0) {
-            mram_write(local_bodies, mram_bodies, sizeof(local_bodies));
-        }
+        
         
     }
+
+    // Kopiere Daten von WRAM zurück in MRAM
+        if (me() == 0) {
+            mram_write(local_bodies, mram_bodies, sizeof(local_bodies));
+            printf("Tasklet %d: Data copied from WRAM to MRAM\n", me());
+        }
     
     return 0;
+}
+
+
+
+
+
+
+
+// math methodes
+float fabsf(float x) {
+    return x < 0 ? -x : x;
+}
+
+
+float sqrtf(float x) {
+    float guess = x / 2.0f;
+    float epsilon = 0.00001f;
+
+    while (fabsf(guess * guess - x) > epsilon) {
+        guess = (guess + x / guess) / 2.0f;
+    }
+
+    return guess;
 }
